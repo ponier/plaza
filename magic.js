fceaae180ffe635d0bc7233fa82ce5020fb70686
Reino Piip <ponier@users.noreply.github.com>
2020-06-11 00:10:16 -0700

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,4 +1,6 @@
-const mapsBase = "https://www.google.com/maps/place/"
+const mapsBase = "https://www.google.com/maps/search/?api=1&query="
+const addressSuffix = "%2C+Sunnyvale%2C+CA+94089"
+
 var spaceNumbers
 
 $.getJSON("data.json", function(json){
@@ -8,6 +10,6 @@ $.getJSON("data.json", function(json){
 function updateLink() {
   const selectedNumber = document.getElementById("space-number").value;
   const mapsAddress = selectedNumber + " " + spaceNumbers[selectedNumber];
-  const queryAddress = mapsAddress.split(" ").join("+") + ",Sunnyvale,CA";
+  const queryAddress = mapsAddress.split(" ").join("+") + addressSuffix;
   document.getElementById("maps-link").innerHTML = "<a href=" + mapsBase + queryAddress + ">" + mapsAddress + "</a>";
 }
