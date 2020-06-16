47fa3aa7d975a0806448a6297006ebe367d78a02
Reino Piip <ponier@users.noreply.github.com>
2020-06-16 15:55:37 -0700

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,16 +1,25 @@
-const mapsBase = "https://www.google.com/maps/search/?api=1&query="
-const addressSuffix = "%2C+Sunnyvale%2C+CA+94089"
+const mapsBase = "https://www.google.com/maps/search/?api=1&query=";
+const addressSuffix = "%2C+Sunnyvale%2C+CA+94089";
 
-var spaceNumbers
+var spaceNumbers;
 
-$.getJSON("data.json", function(json){
-  spaceNumbers = json
-})
+loadJSON();
+
+function loadJSON() {
+  let xhr = (window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"));
+  xhr.onreadystatechange = () => {
+    if (this.readyState === 4 && this.status === 200) {
+        spaceNumbers = JSON.parse(this.responseText);
+    };
+  };
+  xhr.open('GET', 'data.json');
+  xhr.send();
+};
 
 function updateLink() {
-  const selectedNumber = document.getElementById("space-number").value;
-  const mapsAddress = selectedNumber + " " + spaceNumbers[selectedNumber];
-  const mapsQuery = mapsAddress.split(" ").join("+") + addressSuffix;
-  const mapsUrl = mapsBase + mapsQuery
+  let selectedNumber = document.getElementById("space-number").value;
+  let mapsAddress = selectedNumber + " " + spaceNumbers[selectedNumber];
+  let mapsQuery = mapsAddress.split(" ").join("+") + addressSuffix;
+  let mapsUrl = mapsBase + mapsQuery
   document.getElementById("maps-link").innerHTML = `<a href="${mapsUrl}">${mapsAddress}</a>`;
-}
+};
